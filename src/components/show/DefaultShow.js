fadddc393550c890a9dc60816b4abbd083ee1b51
virender <sidhu2772000@gmail.com>
2021-07-31 15:04:26 +0530

    styled the entire app

---
+++ src/components/show/DefaultShow.js
@@ -0,0 +1,43 @@
+/* eslint-disable arrow-body-style */
+import React, { useState, useEffect } from 'react';
+import { getApi } from '../../misc/config';
+import { useShows } from '../../misc/custom-hooks';
+import ShowGrid from './ShowGrid';
+
+const Starred = () => {
+  const [starred] = useShows();
+
+  const [shows, setShows] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => getApi(`/shows/${showId}`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <>
+      {isLoading && <div>Shows are still loading</div>}
+      {error && <div>Error occured: {error}</div>}
+      {!isLoading && !shows && <div>No shows were added</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </>
+  );
+};
+
+export default Starred;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,13 @@ import MainPageLayout from '../components/MainPageLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import { getApi } from '../misc/config';
 import { useLastQuery } from '../misc/custom-hooks';
+import DefaultShow from '../components/show/DefaultShow';
+import {
+  RadioInputsWrapper,
+  SearchButtonWrapper,
+  SearchInput,
+} from './Home.styled';
+import CustomRadio from '../components/CustomRadio';
 
 const Home = () => {
   const [input, setInput] = useLastQuery();
@@ -41,7 +48,7 @@ const Home = () => {
       );
     }
 
-    return null;
+    return <DefaultShow />;
   };
 
   const onRadioChange = ev => {
@@ -50,39 +57,41 @@ const Home = () => {
 
   return (
     <MainPageLayout>
-      <input
+      <SearchInput
         type="text"
         placeholder="Search"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
       />
 
-      <div>
-        <label htmlFor="shows-search">
-          shows
-          <input
+      <RadioInputsWrapper>
+        <div>
+          <CustomRadio
+            label="shows"
             id="shows-search"
             type="radio"
             value="shows"
             checked={isSearchShow}
             onChange={onRadioChange}
           />
-        </label>
-        <label htmlFor="people-search">
-          people
-          <input
+        </div>
+        <div>
+          <CustomRadio
+            label="actors"
             id="people-search"
             type="radio"
             value="people"
             checked={!isSearchShow}
             onChange={onRadioChange}
           />
-        </label>
-      </div>
+        </div>
+      </RadioInputsWrapper>
 
-      <button type="button" onClick={onSearch}>
-        Search
-      </button>
+      <SearchButtonWrapper>
+        <button type="button" onClick={onSearch}>
+          Search
+        </button>
+      </SearchButtonWrapper>
       {renderResults()}
     </MainPageLayout>
   );
